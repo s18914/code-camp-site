be6d6237000bba34c26fd825b573affddb990a18
s18914 <s18914@pjwstk.edu.pl>
2022-11-13 18:17:46 +0100

    pass data between components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import { data } from "../SpeakerData";
+import Speaker from "../src/components/Speaker"
 
 const IndexPage =() => {
+
+  const {id, bio, first,last, favourite, twitterHandle, company, sessions, } = data[0];
   return (
-    <div>Index</div>
+    <div className="container speakers-list">
+      <div className="row">
+        {data.map((speaker) => {
+          return(
+            <Speaker key={speaker.id} speaker={speaker}/>
+          )
+        })}
+        
+      </div>
+    </div>
   )
 }
 
